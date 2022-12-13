791ad12a42dd70d66aac550863ec288561e8d6f7
kayan <paranormalt7@gmail.com>
2022-12-13 19:36:18 -0300

    menu whale

--- whalemap.py
+++ whalemap.py
@@ -1,3 +1,35 @@
-import * from portscan.py 
+from portscan import *
+import sys
 
-nmap = 
+class WhaleMap(object):
+
+    def main(self):
+        print("""
+            ██╗    ██╗██╗  ██╗ █████╗ ██╗     ███████╗    ███╗   ███╗ █████╗ ██████╗ 
+            ██║    ██║██║  ██║██╔══██╗██║     ██╔════╝    ████╗ ████║██╔══██╗██╔══██╗
+            ██║ █╗ ██║███████║███████║██║     █████╗      ██╔████╔██║███████║██████╔╝
+            ██║███╗██║██╔══██║██╔══██║██║     ██╔══╝      ██║╚██╔╝██║██╔══██║██╔═══╝ 
+            ╚███╔███╔╝██║  ██║██║  ██║███████╗███████╗    ██║ ╚═╝ ██║██║  ██║██║     
+            ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     
+                                                __       __
+                                                    '.'--.--'.-'
+                                    .,_------.___,   /' r'
+                                    ', '-._a      '-' .'
+                                    '.    '-'Y \._  /   by kayantchian
+                                        '--;____'--.'-,1
+                                    hi  /..'       '''        
+        """)
+        while True:
+            try:
+                op = int(input('[1] Port Scan\n[2] DNS Enum\n[3] Exit\n>> '))
+                if(op==3):
+                    sys.exit(0)
+                if(op==1):
+                    host = input("Host: ")
+                    portscan = Scan(host)
+                    portscan.tcpscan()
+            except ValueError:
+                pass
+
+whale = WhaleMap()
+whale.main()
