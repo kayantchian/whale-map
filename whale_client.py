991a0e6ee153256c57d7d2406484b0b5f4fcfa74
kayan <paranormalt7@gmail.com>
2022-12-30 20:45:52 -0300

    whale client1

---
+++ whale_client.py
@@ -0,0 +1,21 @@
+import socket
+
+class Client():
+
+    def validatehost(self,host):
+        try:
+            if host.replace('.', '').isdigit():
+                host = socket.gethostbyaddr(host)
+                print(f"\nName server > {host[0]}\n") 
+                #returns a list where 0 position is host name
+            else:
+                if "www." in host:
+                    host = host[4:]
+                    pass
+                if ".com" not in host:
+                    host = host + ".com"
+                    pass
+                print(f"\nIPv4 > {socket.gethostbyname(host)}\n")
+        except socket.herror or socket.gaierror:
+                pass
+        return host
